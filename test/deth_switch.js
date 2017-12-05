36b98354acaf8c8e9ef932b3bbb6e135fbf56a6a
Anyhowclick <howeilun@hotmail.com>
2017-12-05 19:14:06 +0800

    Added test for DSF and minor edit to DSF contract

--- test/deth_switch.js
+++ test/deth_switch.js
@@ -1,6 +1,7 @@
 var DethSwitch = artifacts.require("./DethSwitch.sol");
 
 contract('DethSwitch', function(accounts) {
+
   it("should assert true", function(done) {
     var deth_switch = DethSwitch.deployed();
     assert.isTrue(true);
