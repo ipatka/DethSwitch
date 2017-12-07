bd438b9a0f09936eea86a89da431fd67f56cbde1
Isaac Patka <ipatka@gmail.com>
2017-12-06 22:28:50 -0800

    all working, i think. changed daytoseconds from modifier to function. tested deploying contract with >10 years expiration and failed as it should. tested deploying contract, setting contract to expire by calling extend(0) and was able to withdraw, good. modified getAllowance to check if parent has enough balance for transfer. if not, returns max that could be transferred for withdrawl

--- contracts/DethSwitchFactory.sol
+++ contracts/DethSwitchFactory.sol
@@ -5,6 +5,7 @@ contract ERC20Token {
     // necessary for DethSwitch to be able to reference functions in another contract
     function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
 }
 
 contract DethSwitch {
@@ -30,25 +31,26 @@ contract DethSwitch {
         _;
     }
 
-    modifier onlyIfExpirationTimeExceeded() {
+    modifier onlyIfExpired() {
         require(now >= expirationTime);
         _;
     }
 
-    modifier expirationTimeNoOverflow(uint256 _timeTillExpiration) {
+    modifier expirationTimeNoOverflow(uint256 _daysTillExpiration) {
         //For now, maximum extension allowed is 10 years
         //also have to check for integer overflow
-        require(_timeTillExpiration <= 10 years && _timeTillExpiration <= 2**256 - 1 - now);
+        uint256 _secondsTillExpiration;
+        _secondsTillExpiration = convertDaysToSeconds(_daysTillExpiration);
+        require(_secondsTillExpiration <= 10 years && _secondsTillExpiration <= 2**256 - 1 - now);
         _;
     }
 
-    modifier convertTimeToSeconds(uint256 _time) {
-        _time *= 1 days;
-        _;
+    function convertDaysToSeconds(uint256 _timeDays) internal returns (uint256 _timeSeconds) {
+        _timeSeconds = _timeDays * 1 days;
+        return _timeSeconds;
     }
 
-    function DethSwitch(address _parent, address _heir, string _contractName, uint256 _timeTillExpiration) 
-    convertTimeToSeconds(_timeTillExpiration)
+    function DethSwitch(address _parent, address _heir, string _contractName, uint256 _daysTillExpiration) 
     {
     /* 
     _parent = wallet address of parent
@@ -59,23 +61,27 @@ contract DethSwitch {
         parent = _parent;
         heir = _heir;
         contractName = _contractName;
-        expirationTime = extendExpirationTime(_timeTillExpiration);
+        updateExpirationTime(_daysTillExpiration);
     }
 
-    function extendExpirationTime(uint256 _timeTillExpiration) 
+    function updateExpirationTime(uint256 _daysTillExpiration)
         internal
-        convertTimeToSeconds(_timeTillExpiration)
-        expirationTimeNoOverflow(_timeTillExpiration)
+        expirationTimeNoOverflow(_daysTillExpiration)
         returns (uint256 _expirationTime)
     {
-        expirationTime = now + _timeTillExpiration;
+        uint256 _secondsTillExpiration;
+        _secondsTillExpiration = convertDaysToSeconds(_daysTillExpiration);
+        _expirationTime = now + _secondsTillExpiration;
+        expirationTime = _expirationTime;
         return expirationTime;
     }
 
-    function extendExpirationTimeByParent(uint _timeTillExpiration) onlyParent returns (uint256 _expirationTime) {
-        //_timeTillExpiration will be converted to seconds by function modifier in extendExpirationTime()
-        expirationTime = extendExpirationTime(_timeTillExpiration);
-        return expirationTime;
+    function extendExpirationTime(uint256 _daysTillExpiration) 
+        onlyParent
+        returns (uint256 _expirationTime)
+    {
+        _expirationTime = updateExpirationTime(_daysTillExpiration);
+        return _expirationTime;
     }
 
     function transferThrough(address _from, address _to, uint256 _value, address _tokenAddr) internal returns (bool success) {
@@ -93,10 +99,15 @@ contract DethSwitch {
         // Find how much this DethSwitch contract is allowed to send on behalf of parent
         ERC20Token tok = ERC20Token(_tokenAddr);
         amount = tok.allowance(parent, address(this));
+        uint256 parentBalance;
+        parentBalance = tok.balanceOf(parent);
+        if (amount > parentBalance) {
+            amount = parentBalance;
+        }
         return amount;
     }
 
-    function withdraw(address _tokenAddr) onlyHeir onlyIfExpirationTimeExceeded returns (bool success) {
+    function withdraw(address _tokenAddr) onlyHeir onlyIfExpired returns (bool success) {
         uint256 amount;
         amount = getAllowance(_tokenAddr);
         if (amount > 0) {
